fd19a8523a032e49257b3423511fefaad9eb56ee
rathodaman <88397772+rathodaman@users.noreply.github.com>
2021-09-09 21:12:30 +0530

    Add files via upload

---
+++ let.js
@@ -0,0 +1,16 @@
+//var use
+var a = 56;
+var a = 94;
+console.log(a);
+
+//let use
+let x = "aman";
+x=78;
+//can not redeclare variable name same .
+let y = 0;
+console.log(x);
+
+//const use
+const z=66;
+//z=95;
+console.log(z);
